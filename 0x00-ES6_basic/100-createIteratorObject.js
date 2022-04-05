1219d5575af2cb696e768ca1220a61fc60196df5
brk-a <fnjakai@outlook.com>
2022-04-04 18:12:52 -0700

    Task 13: try v 2.2

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+    const array1 = [];
+  /* eslint-disable */
+    for (const [department, employees] of Object.entries(report.allEmployees)) {
+	for (const emp of employees) {
+	    array1.push(emp);
+	}
+    }
+
+    return array1;
+}
