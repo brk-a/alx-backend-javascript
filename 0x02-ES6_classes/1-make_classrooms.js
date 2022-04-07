61d090a40c4f3a12643cd6e83a91c5707e6ccd97
brk-a <fnjakai@outlook.com>
2022-04-07 00:42:37 -0700

    Tasks 0 & 1: try v 1.0

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+    const room_sizes = [19, 20, 34];
+    const classArr = [];
+
+    for (const size of room_sizes) {
+	classArr.push(new ClassRoom(size));
+    }
+    return classArr;
+}
