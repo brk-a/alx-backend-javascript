90a2cd749dd3e93bb8c723df1ecd03d627134a3f
brk-a <fnjakai@outlook.com>
2022-04-07 06:26:51 -0700

    Task 7: try v 1.2

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default class Airport {
+    constructor(name, code) {
+	this._code = code;
+	this._name = name;
+    }
+    get [Symbol.toStringTag]() {
+	return `${this._code}`;
+    }
+}
